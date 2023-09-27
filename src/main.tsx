6427506941663c5bbc7894d764d14286b1c6ea66
Maciel <macielbmo@gamil.com>
2023-09-27 16:56:37 -0300

    Implementamos um sistema de proteção para a rota de Admin, agora quem não está logado não consegue acessar a rota Admin. Alteramos o nosso arquivos App.tsx para Router.tsx e enviamos para pasta routes, onde tem toda a configuração da rota e da proteção de login.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import {RouterProvider} from 'react-router-dom'
-import { router } from './App'
+import { router } from './routes/Router'
 
 import './index.css'
 
